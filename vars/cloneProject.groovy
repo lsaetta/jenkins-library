116c09f0ad9f04457059e1b7c9aa8dea0bacfdc8
Lou Saetta <l.saetta@geosystems.it>
2023-07-29 18:56:12 +0200

    d

--- vars/cloneProject.groovy
+++ vars/cloneProject.groovy
@@ -1,8 +1,12 @@
 def call(String branchName, String credId, String gitUrl){
-	steps {
-		git branch: branchName, changelog: false, credentialsId: credId, poll: true, url: gitUrl
-		dir('kubernetes') {
-			git branch: 'development', changelog: false, credentialsId: credId, poll: true, url: 'git@geogitlab1.intersistemi.it:geosystems/kubernates.git'
+    stages {
+		stage('cloneProject') {
+			steps {
+				git branch: branchName, changelog: false, credentialsId: credId, poll: true, url: gitUrl
+				dir('kubernetes') {
+					git branch: 'development', changelog: false, credentialsId: credId, poll: true, url: 'git@geogitlab1.intersistemi.it:geosystems/kubernates.git'
+				}
+			}
 		}
-	}
+    }
 }
